c4731d43b956d7af6b4274d5af122584d6fed611
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-05-22 21:41:58 +0200

    Create component that includes a user login panel

---
+++ src/Components/LoginPanel.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+function LoginPanel({ onLogin }) {
+    const [fname, setName] = useState({
+        fName: "",
+        password: "",
+    });
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+
+        setName(prevValue => {
+            return {
+                ...prevValue,
+                [name]: value
+            };
+        });
+    }
+
+    function login(event) {
+        event.preventDefault();
+        onLogin(fname.fName);
+    }
+
+    return (
+        <div className="container">
+            <h1>
+                Log In ↓
+            </h1>
+            <form onSubmit={login}>
+                <input
+                    onChange={handleChange}
+                    name="fName"
+                    value={fname.fName}
+                    placeholder="First Name"
+                    required
+                />
+                <input
+                    type='password'
+                    name="password"
+                    placeholder="Password"
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default LoginPanel;
